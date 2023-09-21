0b34a5423298ac9c9dd299f7238233b21220b2b9
Leonardo Genzolini <leonardogenzolini@gmail.com>
2023-09-21 11:48:20 +0200

    useEffect and get quote from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  // https://api.quotable.io/random
+
+  useEffect(() => {
+
+  })
+
   return (
     <div className="App">
       <div id="quote-box">
         <p id="text"></p>
-
+        <p id="author"></p>
+        <button id="new-quote">New Quote</button>
+        <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">Tweet Quote</a>
       </div>
     </div>
   );
