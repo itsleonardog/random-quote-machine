46f0951722dd5d4cc2b0bf5cba44f697b79675b4
Leonardo Genzolini <leonardogenzolini@gmail.com>
2023-09-21 20:15:31 +0200

    page styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
 
@@ -24,9 +25,9 @@ function App() {
     <div className="App">
       <div id="quote-box">
         <p id="text">{quoteInfo.text}</p>
-        <p id="author">{quoteInfo.author}</p>
+        <p id="author">- {quoteInfo.author}</p>
         <button id="new-quote" onClick={getQuote}>New Quote</button>
-        <a id="tweet-quote" href={"https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text" + quoteInfo.text}>Tweet this</a>
+        <a id="tweet-quote" href={"https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text" + quoteInfo.text}><i class="fa-brands fa-square-x-twitter fa-2x"></i></a>
       </div>
     </div>
   );
