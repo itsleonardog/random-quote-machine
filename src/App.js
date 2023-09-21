22aebc4c9c3250cdc663192ddc0274973bb76b1a
Leonardo Genzolini <leonardogenzolini@gmail.com>
2023-09-21 12:07:05 +0200

    render quote text and author in the UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [quoteInfo, setQuoteInfo] = useState({})
 
   useEffect(() => {
     getQuote();
@@ -11,15 +13,18 @@ function App() {
     fetch('https://api.quotable.io/random')
       .then(response => response.json())
       .then((data) => {
-        console.log(data);
+        setQuoteInfo({
+          text: data.content,
+          author: data.author
+        })
       });
   }
 
   return (
     <div className="App">
       <div id="quote-box">
-        <p id="text"></p>
-        <p id="author"></p>
+        <p id="text">{quoteInfo.text}</p>
+        <p id="author">{quoteInfo.author}</p>
         <button id="new-quote">New Quote</button>
         <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">Tweet Quote</a>
       </div>
