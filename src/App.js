427c60e2caaf4e7ab4cedcbcca981e65e6e7c844
Leonardo Genzolini <leonardogenzolini@gmail.com>
2023-09-21 12:22:33 +0200

    app finalized

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
         <p id="text">{quoteInfo.text}</p>
         <p id="author">{quoteInfo.author}</p>
         <button id="new-quote" onClick={getQuote}>New Quote</button>
-        <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">Tweet Quote</a>
+        <a id="tweet-quote" href={"https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text" + quoteInfo.text}>Tweet this</a>
       </div>
     </div>
   );
